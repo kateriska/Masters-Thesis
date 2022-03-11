026e4529249e77cf069202894fa20c81ecc5b9ee
katerina <xforto00@vutbr.cz>
2022-03-11 10:16:42 +0100

    CenternetResnet added to options

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from model import UsedModel
 
 def parse_args():
     argParser = argparse.ArgumentParser(description='Train model for detection and classification of diseased fingerprints')
-    argParser.add_argument('--model', dest='model', action='store', default="ssd_mobilenet_v2", choices=['ssd_mobilenet_v2', 'faster_rcnn_resnet50', 'faster_rcnn_resnet101', 'ssd_resnet50', 'efficient_det_d0', 'efficient_det_d1', 'centernet_hourglass'], help='Select type of model.')
+    argParser.add_argument('--model', dest='model', action='store', default="ssd_mobilenet_v2", choices=['ssd_mobilenet_v2', 'faster_rcnn_resnet50', 'faster_rcnn_resnet101', 'ssd_resnet50', 'efficient_det_d0', 'efficient_det_d1', 'centernet_hourglass', 'centernet_resnet101'], help='Select type of model.')
     argParser.add_argument('--epochs', dest='epochs', default=25000, action='store', type=int, help='Number of epochs to train for.')
     argParser.add_argument('--test', dest='test', action='store_true', default=False, help='Run model on test folder, detect and clasify diseases.')
     return argParser.parse_args()
